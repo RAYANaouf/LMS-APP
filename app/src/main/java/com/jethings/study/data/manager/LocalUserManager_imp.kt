2b3b15006acd4e98ddebf77089bd7bf93ab1d7b6
rayan_aouf <rayanaouf1512@gmail.com>
2025-05-27 22:09:51 +0100

    progressing...

--- app/src/main/java/com/jethings/study/data/manager/LocalUserManager_imp.kt
+++ app/src/main/java/com/jethings/study/data/manager/LocalUserManager_imp.kt
@@ -1,10 +1,10 @@
 package com.jethings.study.data.manager
 
 import com.jethings.study.data.db.entities.Account
-import com.jethings.study.domain.manager.LocalAccountManager
+import com.jethings.study.domain.manager.LocalUserManager
 import kotlinx.coroutines.flow.Flow
 
-class LocalAccountManager_imp : LocalAccountManager {
+class LocalUserManager_imp : LocalUserManager {
     override suspend fun saveAccount(account: Account) {
         TODO("Not yet implemented")
     }
--- app/src/main/java/com/jethings/study/domain/manager/LocalUserManager.kt
+++ app/src/main/java/com/jethings/study/domain/manager/LocalUserManager.kt
@@ -4,13 +4,13 @@ package com.jethings.study.domain.manager
 import com.jethings.study.data.db.entities.Account
 import kotlinx.coroutines.flow.Flow
 
-interface LocalAccountManager {
+interface LocalUserManager {
 
     suspend fun saveAccount(account : Account)
 
 
     suspend fun logInAccount(email : String, password : String)   : Account?
-    suspend fun  logOutAccount()                          : Boolean
+    suspend fun logOutAccount()                          : Boolean
 
     fun isLogInAccount() : Account?
     fun getAllAccounts() : Flow<List<Account>>
